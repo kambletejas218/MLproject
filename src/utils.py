cb907e4e50424744e46dc4eae39c3029da17dd34
kambletejas218 <kambletejas218@gmail.com>
2024-11-09 13:36:38 +0530

    Application

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill 
+import pickle
 
 from sklearn.metrics import r2_score
 from src.exception import CustomException
@@ -47,5 +48,17 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
 
         return report
 
-    except :
-        pass
+    except Exception as e :
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+        
+        
